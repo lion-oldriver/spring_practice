022c6e6588aeca07a2352875019b3ec37c51f4e2
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-06 17:59:15 +0900

    section3_finish

--- Review/src/service/MemberServiceImpl.java
+++ Review/src/service/MemberServiceImpl.java
@@ -5,7 +5,15 @@ import java.util.ArrayList;
 import entity.Member;
 
 public class MemberServiceImpl implements MemberService {
-
+	
+	private static MemberServiceImpl singleton = new MemberServiceImpl();
+    
+	private MemberServiceImpl() {};
+	
+	public static MemberServiceImpl getInstance() {
+		return singleton;
+	}
+	
 	@Override
 	public String greet(int i) {
 		String[] greetings = {"Good Morning", "Hello", "Good Evening"};
@@ -22,5 +30,16 @@ public class MemberServiceImpl implements MemberService {
 		
 		return list;
 	}
+
+	@Override
+	public int sumOf(int x, int y) {
+		int sum = 0;
+		for(int i = x; i <= y; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+	
+	
 
 }
